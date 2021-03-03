1e9b115f400a2a46f02679f4aef0503c01e8c3da
kimtaegwan <ab154@naver.com>
2021-03-03 18:10:47 +0900

    Use firebase realtime database - sync cards

--- src/components/MakerComponent/index.js
+++ src/components/MakerComponent/index.js
@@ -8,51 +8,31 @@ import Header from "../Shared/Header";
 import Editor from "./Editor";
 import Preview from "./Preview";
 
-const MakerComponent = ({ FileInput, authService }) => {
-  const [cards, setCards] = useState({
-    1: {
-      id: "1",
-      name: "Ellie",
-      company: "Samsung",
-      theme: "Dark",
-      title: "Software Engineer",
-      email: "ellie@gmail.com",
-      message: "go for it",
-      fileName: "ellie",
-      fileURL: null,
-    },
-    2: {
-      id: "2",
-      name: "Ellie2",
-      company: "Samsung",
-      theme: "Light",
-      title: "Software Engineer",
-      email: "ellie@gmail.com",
-      message: "go for it",
-      fileName: "ellie",
-      fileURL: "ellie.png",
-    },
-    3: {
-      id: "3",
-      name: "Ellie3",
-      company: "Samsung",
-      theme: "Colorful",
-      title: "Software Engineer",
-      email: "ellie@gmail.com",
-      message: "go for it",
-      fileName: "ellie",
-      fileURL: null,
-    },
-  });
-
+const MakerComponent = ({ FileInput, authService, cardRepository }) => {
   const history = useHistory();
+  const historyState = history?.location?.state;
+  const [cards, setCards] = useState({});
+  const [userId, setUserID] = useState(historyState && historyState.id);
+
   const onLogout = () => {
     authService.logout();
   };
 
   useEffect(() => {
+    if (!userId) {
+      return;
+    }
+    const stopSync = cardRepository.syncCards(userId, cards => {
+      setCards(cards);
+    });
+    return () => stopSync();
+  });
+  useEffect(() => {
+    //로그인
     authService.onAuthChange(user => {
-      if (!user) {
+      if (user) {
+        setUserID(user.uid);
+      } else {
         history.push("/");
       }
     });
@@ -64,6 +44,7 @@ const MakerComponent = ({ FileInput, authService }) => {
       updated[card.id] = card;
       return updated;
     });
+    cardRepository.saveCard(userId, card);
   };
 
   const deleteCard = card => {
@@ -72,6 +53,7 @@ const MakerComponent = ({ FileInput, authService }) => {
       delete updated[card.id];
       return updated;
     });
+    cardRepository.removeCard(userId, card);
   };
 
   return (
--- src/index.js
+++ src/index.js
@@ -5,17 +5,23 @@ import App from "./App";
 
 import AuthService from "./service/auth_service";
 import ImageUploader from "./service/image_uploader";
+import CardRepository from "./service/card_repository";
 import ImageFileInput from "./components/MakerComponent/Editor/ImageFileInput";
 
 const authService = new AuthService();
 const imageUploader = new ImageUploader();
+const cardRepository = new CardRepository();
 const FileInput = props => (
   <ImageFileInput {...props} imageUploader={imageUploader} />
 );
 
 ReactDOM.render(
   <React.StrictMode>
-    <App authService={authService} FileInput={FileInput} />
+    <App
+      authService={authService}
+      FileInput={FileInput}
+      cardRepository={cardRepository}
+    />
   </React.StrictMode>,
   document.getElementById("root")
 );
