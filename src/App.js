6304eac6840af5f684dd4881244aebede18d4e3d
kimtaegwan <ab154@naver.com>
2021-03-16 16:21:23 +0900

    Edit router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App({ FileInput, authService, cardRepository }) {
     <div className={styles.app}>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/card-maker">
             <Login authService={authService} />
           </Route>
           <Route path="/maker">
